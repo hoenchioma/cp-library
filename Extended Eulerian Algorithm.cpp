f3935cfbaebb2cfb30a4c23a15c0d347dd929195
Raheeb Hassan <raheeb998@gmail.com>
2019-09-28 13:02:38 +0600

    fixed mistake in Extended Euclidean Algo and added FFT

--- Extended Eulerian Algorithm.cpp
+++ Extended Eulerian Algorithm.cpp
@@ -1,26 +1,22 @@
-// Ternary search
-// (Maximizes a unimodal function within a range)
-// O(log3(n))
+// Extended Euclidean Algorithm
+// calculates gcd(a, b) along with x and y such that a⋅x+b⋅y=gcd(a,b)
+// O(log(min(a, b))
 
 #include <bits/stdc++.h>
 using namespace std;
 
-double func(double x) { // function to maximize
-	return -1*1*x*x + 2*x +3; 
-}
-
-// find maximum value in unimodal function
-double ts(double start, double end)
-{
-    double l = start, r = end;
-
-    for(int i=0; i<200; i++) { // precision = (1/3) ^ iterations
-      double l1 = (l*2+r)/3;
-      double l2 = (l+2*r)/3;
-      //cout<<l1<<" "<<l2<<endl;
-      if(func(l1) > func(l2)) r = l2; else l = l1;
+int gcd(int a, int b, int & x, int & y) {
+    if (a == 0) {
+        x = 0;
+        y = 1;
+        return b;
     }
-
-    double x = l;
-    return func(x);
+    int x1, y1;
+    int d = gcd(b % a, a, x1, y1);
+    x = y1 - (b / a) * x1;
+    y = x1;
+    return d;
 }
+
+// lcm(a, b) = ((a * b) / gcd(a, b)) or (a / gcd(a, b) * b) (to avoid integer overflow)
+// gcd of fraction = gcd of numerators / lcm of denominators
